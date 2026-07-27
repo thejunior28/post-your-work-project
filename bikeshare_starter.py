8f4550fc1de808d64ab8c1e5f73e041ce235891e
Richard Castro <richard.castro@unesp.br>
2026-07-27 14:00:05 -0300

    Improve comment for city selection

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -16,8 +16,10 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
+    # Ask the user to choose a city and validate the input
+
+    
 
     # get user input for month (all, january, february, ... , june)
 
